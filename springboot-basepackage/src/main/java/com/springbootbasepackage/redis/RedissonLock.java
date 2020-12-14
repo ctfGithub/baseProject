04ff73eff745adff0ad1252319e24479849757e0
shutengfei <i_stfei@3songshu.com>
2020-12-14 21:03:59 +0800

    redis 连接使用--redisson枷锁

--- springboot-basepackage/src/main/java/com/springbootbasepackage/redis/RedissonLock.java
+++ springboot-basepackage/src/main/java/com/springbootbasepackage/redis/RedissonLock.java
@@ -6,47 +6,20 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.params.SetParams;
 
 import java.util.concurrent.TimeUnit;
 
-
-/**
- * 连接redis
- */
-public class RedisClass {
-
-    @Autowired
-    private RedissonClient redissonClient;
-
-    @Autowired
-    private RedisLockService redisLockService;
-
-    @Autowired
-    private SedisManage redisManager;
-
-
+public class RedissonLock {
 
     @Test
-    public void test() {
-        //实例化一个客户端
-        Jedis jedis = new Jedis("127.0.0.1");
-        //ping下，看看是否通的
-        System.out.println("redis运行状态: " + jedis.ping());
-
-
-        //设置永久性的key-value，不用添加时间处理
-        jedis.set("leiTest", "测试使用");
-
-        //设置key 10 s
-        String result = jedis.set("leiTestTime", "设置这个key为10s", new SetParams().ex(60));
-        System.out.println("result: " + result);
-
+    public void redissonLock(){
+        boolean  s = false;
         //枷锁的第一种方法，Redisson枷锁
         Config config = new Config();
+
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+//        config.useClusterServers().addNodeAddress("redis://127.0.0.1:6379").addNodeAddress("redis://127.0.0.1:6379").setPassword("").setScanInterval(1000);
+
         RedissonClient redissonClient = Redisson.create(config);
         RedissonReactiveClient reactive = Redisson.createReactive(config);
         RLock lock = redissonClient.getLock("redis+key+ceshi");
@@ -55,6 +28,8 @@ public class RedisClass {
             boolean res = lock.tryLock(3, 600, TimeUnit.SECONDS);
             if(res){    //成功
                 System.out.println("你好");
+                s= true;
+                System.out.println(s);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -62,15 +37,8 @@ public class RedisClass {
             lock.unlock();
             reactive.shutdown();
         }
-
-
-        
-
-
-
-
-
     }
+
 
 
 }
