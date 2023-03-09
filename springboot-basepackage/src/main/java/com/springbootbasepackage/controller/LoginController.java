d0b2e2fc9bb4e2654ddb65ab0bf99614b0022219
caotengfei <1053645417@qq.com>
2023-03-09 10:36:43 +0800

    手机验证码

--- springboot-basepackage/src/main/java/com/springbootbasepackage/controller/LoginController.java
+++ springboot-basepackage/src/main/java/com/springbootbasepackage/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.springbootbasepackage.controller;
 
 import com.springbootbasepackage.base.SntResult;
+import com.springbootbasepackage.dto.LoginIphoneAndYzmDTO;
 import com.springbootbasepackage.dto.LoginIphoneDTO;
 import com.springbootbasepackage.service.LoginService;
 import io.swagger.annotations.Api;
@@ -24,8 +25,19 @@ public class LoginController {
     @PostMapping("/sendIphone")
     @ApiOperation("发送手机号生成验证码")
     public SntResult<String> sendIphone (@RequestBody LoginIphoneDTO loginIphoneDTO){
-        String token  = loginService.sendIphone(loginIphoneDTO);
+        String yzm  = loginService.sendIphone(loginIphoneDTO);
+        return SntResult.ok(yzm);
+    }
+
+
+
+    //2、手机号和 验证码生成token
+    @PostMapping("/tokenCreate")
+    @ApiOperation("登录")
+    public SntResult<String> tokenCreate (@RequestBody LoginIphoneAndYzmDTO loginIphoneAndYzmDTO){
+        String token  = loginService.tokenCreate(loginIphoneAndYzmDTO);
         return SntResult.ok(token);
     }
+
 
 }
