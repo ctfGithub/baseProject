e9c6840a65b7b38d000b00beb12744ced363f475
caotengfei <1053645417@qq.com>
2024-07-17 10:32:46 +0800

    fegin 接口 多服务 链路 requestId

--- springboot-basepackage/src/main/java/com/springbootbasepackage/configuration/RequestIdInterceptor.java
+++ springboot-basepackage/src/main/java/com/springbootbasepackage/configuration/RequestIdInterceptor.java
@@ -8,13 +8,12 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
 public class RequestIdInterceptor implements RequestInterceptor {
     public static final String TRACE_ID_HEADER = "requestId";
-
-    private static final ThreadLocal<String> requestId = new ThreadLocal<>();
 
     @Override
     public void apply(RequestTemplate template) {
@@ -22,16 +21,19 @@ public class RequestIdInterceptor implements RequestInterceptor {
 
         // 假设traceId是从某个地方获取，这里只是示例
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-        // 从请求头中获取requestId
-        String requestId  = (String) request.getAttribute("requestId");
-        if(StringUtils.isBlank(requestId)){
-            requestId = UUID.randomUUID().toString(); // 应该从合适的上下文获取或生成
+        if(Objects.nonNull(attributes)){
+            HttpServletRequest request = attributes.getRequest();
+            // 从请求头中获取requestId
+            String requestId  = (String) request.getAttribute("requestId");
+            if(StringUtils.isBlank(requestId)){
+                requestId = UUID.randomUUID().toString(); // 应该从合适的上下文获取或生成
+            }
+            //requestId 放在 请求头中
+            request.setAttribute("requestId",requestId);
+            log.info("requestId:{}",requestId);
+            template.header(TRACE_ID_HEADER, requestId);
         }
-        //requestId 放在 请求头中
-        request.setAttribute("requestId",requestId);
-        log.info("requestId:{}",requestId);
-        template.header(TRACE_ID_HEADER, requestId);
+
     }
 
 
