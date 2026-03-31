5b3901c93a9ab7915012e496294e389ca9ed2f95
tengfei.cao <tengfei.cao@budweiserapac.com>
2026-03-31 09:20:15 +0800

    xxl-job 和nacos 配置中心

--- springboot-basepackage/src/main/java/com/springbootbasepackage/SpringbootApplication.java
+++ springboot-basepackage/src/main/java/com/springbootbasepackage/SpringbootApplication.java
@@ -1,6 +1,5 @@
 package com.springbootbasepackage;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @ComponentScan(basePackages ="com.springbootbasepackage.*")
 @EnableDiscoveryClient
-@NacosConfigurationProperties(dataId = "${spring.application.name}.yaml")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
